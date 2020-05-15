7665f55a389f3001c511b1c3e0258cbd52133b20
Davi Marques <davimarques19@gmail.com>
2020-05-15 09:39:33 -0300

    Registrando componente e serviço

--- src/main/java/com/davimarques/curso/resources/UserResource.java
+++ src/main/java/com/davimarques/curso/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.davimarques.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.davimarques.curso.entities.User;
+import com.davimarques.curso.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> fildAll(){
-		User u = new User(1L, "João", "joao@gmail.com", "1111122222", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> fildAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> fildById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
